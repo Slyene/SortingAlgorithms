ffd8907a1c4843665433c19b59a50061a1e8b21c
Slyene <deerektor@gmail.com>
2023-01-10 17:08:56 +0300

    Main.kt changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
-    
+    val a = MergeSort.sort(IntArray(70) { _ ->  (Math.random() * 100).toInt() })
+    val b = CountingSort.sort(IntArray(70) { _ ->  (Math.random() * 100).toInt() })
+    a.forEach { print("$it ") }
+    println()
+    b.forEach { print("$it ") }
 }
